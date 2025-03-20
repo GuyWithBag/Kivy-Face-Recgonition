4c53994024d99f574b6f25e35cac6b7e48c16434
GuyWithBag <ldulaugon123@gmail.com>
2025-03-20 10:39:26 +0800

    Added mani.py and fixed nutrition and recipe

--- recipies.py
+++ recipies.py
@@ -2,22 +2,22 @@ import requests
 
 def get_recipes(fruit_name): # Returns array
 	api_key = "c9becf792f5648968445e5f885b12401" # Replace api key from spoonacular
-	uri = "https://api.spoonacular.com/recipies/complexSearch" # Rep
+	uri = "https://api.spoonacular.com/recipes/complexSearch" # Rep
 	params = {
 		"query": fruit_name,
 		"apiKey": api_key,
 		"number": 8 # NUmber of recipies to retrieve
 	}
-	resopnse = requests.get(uri, params=params)
+	response = requests.get(uri, params=params)
 	
 	# Check if code has errors
 	if response.status_code == 200:
-		recipies = response.json()["results"]
-		return (recipe("title") for recipe in recipes) # return recipe titles
+		recipes = response.json()["results"]
+		return [recipe["title"] for recipe in recipes] # return recipe titles
 	else: 
 		return ["No recipies found. "] # Handle errors
 
 # Example usage
 fruit_name = "carrot"
-recipies = get_recipies(fruit_name)
-print(f"Recipies for {fruit_name}: {recipies}")
+recipes = get_recipes(fruit_name)
+print(f"Recipes for {fruit_name}: {recipes}")
