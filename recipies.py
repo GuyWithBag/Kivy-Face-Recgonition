ac4688b43c5fc0903864aac8eb40aa8068c01d43
GuyWithBag <ldulaugon123@gmail.com>
2025-03-20 10:19:59 +0800

    Added url

--- recipies.py
+++ recipies.py
@@ -2,7 +2,7 @@ import requests
 
 def get_recipes(fruit_name): # Returns array
 	api_key = "c9becf792f5648968445e5f885b12401" # Replace api key from spoonacular
-	uri = "" # Rep
+	uri = "https://api.spoonacular.com/recipies/complexSearch" # Rep
 	params = {
 		"query": fruit_name,
 		"apiKey": api_key,
