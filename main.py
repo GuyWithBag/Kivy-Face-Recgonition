0bc6db4e84a948bda61c6ff18bd95a8d023f035a
GuyWithBag <ldulaugon123@gmail.com>
2025-03-20 10:54:06 +0800

    Added bulldozer.spec and kivy works now

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class MainApp(App): 
+    def build(self): 
+        layout = BoxLayout(orientation="vertical")
+        return layout
+    
+if __name__ == "__main__": 
+    MainApp().run()
